0e20f3dc29ac68f0e48c132906b1eaeea3c579ae
Fernando Funez <ferfunes@gmail.com>
2019-08-21 22:53:41 +0000

    css working

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ window.onload = () => {
 }
 let generateExcuse = () =>{
 
-var who = ['the dog','my granma','his turtle','my bird'];
+var who = ['the cop','my 4geeks academy teacher','his turtle','my bird'];
 var what = ['eat','pissed','crushed','broked'];
 var when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
-var where = ['on the street', 'in the park', 'at the office'];
+var where = ['on the street', 'in the park', 'at the office', 'in the train'];
 
 var whoIndex = Math.floor(Math.random() * who.length);
 var whatIndex = Math.floor(Math.random() * what.length);
